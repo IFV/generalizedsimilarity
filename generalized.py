c65de7beac27b4abeca1a42dbfa1812401f491f3
Dmitry Zinoviev <dzinoviev@gmail.com>
2016-03-11 22:53:38 -0500

    Initial revision.

---
+++ generalized.py
@@ -0,0 +1,48 @@
+# Implemented by Dmitry Zinoviev <dzinoviev@suffolk.edu>
+
+import numpy as np
+import networkx as nx
+
+def generalized_similarity(graph, min_eps=0.01, max_iter=50):
+    """
+    Calculate generalized similarities between nodes in a BIPARTITE 
+    graph, as described in Balázs Kovács, "A generalized model of 
+    relational similarity," Social Networks, 32(3), July 2010, pp. 197–211.
+
+    Raise a ValueError exception if the graph is not bipartite.
+
+    Return two weighted graphs of similarities, attained precision, 
+    and the number of iterations.
+    """
+
+    if not nx.is_bipartite(graph):
+        raise ValueError("Not a bipartite graph")
+
+    s = nx.bipartite.sets(graph)
+    arcs = nx.bipartite.biadjacency_matrix(graph, s[0], s[1]).toarray()
+
+    arcs0 = arcs - arcs.mean(axis=1)[:, np.newaxis]
+    arcs1 = arcs.T - arcs.mean(axis=0)[:, np.newaxis]
+
+    eps = min_eps + 1
+    N = np.eye(arcs.shape[1])
+
+    iters = 0
+    
+    while eps > min_eps and iters < max_iter:
+        M = arcs0.dot(N).dot(arcs0.T)
+        m = np.sqrt(M.diagonal())
+        M = ((M / m).T / m).T
+        
+        Np = arcs1.dot(M).dot(arcs1.T)
+        n = np.sqrt(Np.diagonal())
+        Np = ((Np / n).T / n).T
+        eps = np.abs(Np - N).max()
+        N = Np
+
+        iters += 1
+
+    f = nx.relabel_nodes(nx.Graph(M), dict(enumerate(s[0])))
+    g = nx.relabel_nodes(nx.Graph(Np), dict(enumerate(s[1])))
+
+    return (f, g, eps, iters)
---
+++ generalized_test.py
@@ -0,0 +1,24 @@
+# Implemented by Dmitry Zinoviev <dzinoviev@suffolk.edu>
+
+import networkx as nx
+import generalized
+
+def test_generalized_similarity():
+    """
+    Calculate the generalized similarities between events and women 
+    in the "classical" Southern Women graph.
+    """
+
+    graph = nx.davis_southern_women_graph()
+    women, events, eps, iters = generalized.generalized_similarity(graph)
+
+    print("Number of iterations:", iters)
+    print("Attained precision:", eps)
+    print("Event network:", len(events), "nodes")
+    print("Least similar events:", 
+          sorted(events.edges(data=True), key=lambda x: x[2]['weight'])[0][0:2])
+    print("Women network:", len(women), "nodes")
+    print("Least similar women:", 
+          sorted(women.edges(data=True), key=lambda x: x[2]['weight'])[0][0:2])
+
+test_generalized_similarity()
